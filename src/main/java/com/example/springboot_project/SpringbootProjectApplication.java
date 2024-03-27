58181b3d4480575857f92e0b372bd6cf61dcfe96
Gabriela Rodriguez Medina <gabriela.medina@neoris.com>
2024-03-27 13:03:48 -0600

    Hello world controller

--- src/main/java/com/example/springboot_project/SpringbootProjectApplication.java
+++ src/main/java/com/example/springboot_project/SpringbootProjectApplication.java
@@ -8,6 +8,7 @@ public class SpringbootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProjectApplication.class, args);
-	}
 
+		System.out.println("Hello World");
+	}
 }
---
+++ src/main/java/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+    public String hello() {
+        return "Hello world";
+    }
+}
