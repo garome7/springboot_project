60854e71be2bb71d72f64ebac219a7031078b2ee
Gabriela Rodriguez Medina <gabriela.medina@neoris.com>
2024-03-27 18:05:38 -0600

    creation of clientRepository and update of SpringBootProjectApplication

--- src/main/java/com/example/springboot_project/SpringbootProjectApplication.java
+++ src/main/java/com/example/springboot_project/SpringbootProjectApplication.java
@@ -2,13 +2,11 @@ package com.example.springboot_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.example.springboot_project")
 @SpringBootApplication
 public class SpringbootProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootProjectApplication.class, args);
-
-		System.out.println("Hello World");
-	}
+	public static void main(String[] args) {SpringApplication.run(SpringbootProjectApplication.class, args);}
 }
---
+++ src/main/java/com/example/springboot_project/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot_project.repository;
+
+import com.example.springboot_project.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
